46f761ba814b609e726cfc9b4563a07539ad9944
Sam Stites <sam@stites.io>
2014-06-26 13:51:51 -0700

    add - jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,18 @@
 module.exports = function(grunt) {
-    grunt.initConfig({});
+    grunt.initConfig({
+        jshint: {
+            all: ['src/**/*.js'],
+            options: {
+                globals: {
+                    _: false,
+                    $: false
+                },
+                browser: true,
+                devel: true
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
 };
