f6590097933d8341d5d1f4728d9804bd3086f0e2
Sam Stites <sam@stites.io>
2014-07-01 14:44:48 -0700

    add - testem

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,36 @@
 module.exports = function(grunt) {
+    var files = ['src/**/*.js', 'test/**/*.js'];
     grunt.initConfig({
         jshint: {
-            all: ['src/**/*.js'],
+            all: files,
             options: {
                 globals: {
                     _: false,
-                    $: false
+                    $: false,
+                    jasmine: false,
+                    describe: false,
+                    it: false,
+                    expect: false,
+                    beforeEach: false,
                 },
                 browser: true,
                 devel: true
             },
+        },
+        testem: {
+            unit: {
+                options: {
+                    framework: 'jasmine2',
+                    launch_in_dev: ['PhantomJS'],
+                    before_tests: 'grunt jshint',
+                    serve_files: files,
+                    watch_files: files
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-testem');
 
 };
