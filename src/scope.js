c1df8029175ae7e9441e8e0f28cb26bf2f76aa65
Sam Stites <sam@stites.io>
2014-07-05 07:34:54 -0700

    style - 2-spaced tabs

--- src/scope.js
+++ src/scope.js
@@ -4,30 +4,31 @@
 function initWatchVal() {}
 
 function Scope() {
-    this.$$watchers = [];
+  this.$$watchers = [];
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn) {
-    var watcher = {
-        watchFn: watchFn,
-        listenerFn: listenerFn,
-        last: initWatchVal
-    };
-    this.$$watchers.push(watcher);
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn,
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
 };
 
 Scope.prototype.$digest = function(){
-    var self = this;
-    var newValue, oldValue;
+  var self = this;
+  var newValue, oldValue;
 
-    _.forEach(this.$$watchers, function(watcher){
-        newValue = watcher.watchFn(self);
-        oldValue = watcher.last;
-        if (newValue !== oldValue) {
-            watcher.last = newValue;
-            watcher.listenerFn(newValue,
-                (oldValue === initWatchVal ? newValue : oldValue),
-                self);
-        }
-    });
+  _.forEach(this.$$watchers, function(watcher){
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newValue;
+      watcher.listenerFn(newValue,
+        (oldValue === initWatchVal ? newValue : oldValue),
+        self
+      );
+    }
+  });
 };
