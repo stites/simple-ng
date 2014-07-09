5d23101b466b810066469589b71dfa94ca9dd369
Sam Stites <sam@stites.io>
2014-07-09 15:42:13 -0400

    it('compares based on value, if enabled')

--- src/scope.js
+++ src/scope.js
@@ -8,10 +8,11 @@ function Scope() {
   this.$$lastDirtyWatch = null;
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn) {
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function(){},
+    valueEq: !!valueEq,
     last: initWatchVal
   };
   this.$$watchers.push(watcher);
@@ -25,9 +26,9 @@ Scope.prototype.$$digestOnce = function(){
   _.forEach(this.$$watchers, function(watcher){
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
-    if (newValue !== oldValue) {
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
       self.$$lastDirtyWatch = watcher;
-      watcher.last = newValue;
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
       watcher.listenerFn(newValue,
         (oldValue === initWatchVal ? newValue : oldValue),
         self
@@ -51,4 +52,8 @@ Scope.prototype.$digest = function(){
       throw '10 digest iterations reached';
     }
   } while (dirty);
+};
+
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+  return (valueEq) ? _.isEqual(newValue, oldValue) : newValue === oldValue;
 };
