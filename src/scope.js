a8f26042f912b0aac436e67e7bdcac57dc1dc842
Sam Stites <sam@stites.io>
2014-07-10 14:14:24 -0400

    it('executes  function later in the same cycle')

--- src/scope.js
+++ src/scope.js
@@ -6,6 +6,7 @@ function initWatchVal() {}
 function Scope() {
   this.$$watchers = [];
   this.$$lastDirtyWatch = null;
+  this.$$asyncQueue = [];
 }
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
@@ -47,6 +48,10 @@ Scope.prototype.$digest = function(){
   var dirty;
   this.$$lastDirtyWatch = null;
   do {
+    while (this.$$asyncQueue.length) {
+      var asyncTask = this.$$asyncQueue.shift();
+      asyncTask.scope.$eval(asyncTask.expression);
+    }
     dirty = this.$$digestOnce();
     if (dirty && !(ttl--)) {
       throw '10 digest iterations reached';
@@ -76,4 +81,8 @@ Scope.prototype.$apply = function(expr) {
   } finally {
     this.$digest();
   }
+};
+
+Scope.prototype.$evalAsync = function(expr){
+  this.$$asyncQueue.push({scope: this, expression: expr});
 };
